87235a61018a9bb5380192f2e762964f1ce37c2a
Marco Guaspari Worms <marcogworms@gmail.com>
2016-10-02 17:21:10 -0300

    better example

--- examples.js
+++ examples.js
@@ -2,24 +2,47 @@ const runBenchmark = require('./index')
 
 const aBenchmark = {
   description: 'sum',
-  iterations: 20,
+  iterations: 10,
   tests: [
     {
-      description: 'sugar',
-      amount: 100000,
+      description: 'Maneira 1',
+      amount: 1000,
       testFunc: (persist) => {
-        var a
-        a += 1
+        var list = Array(100000).fill().map((x, index) => index)
       }
     },
     {
-      description: 'nosugar',
-      amount: 100000,
+      description: 'Maneira 2',
+      amount: 1000,
       testFunc: (persist) => {
-        var a
-        a = a + 1
+        var list = [...Array(100000).keys()]
       }
     },
+    {
+      description: 'Maneira 3',
+      amount: 1000,
+      testFunc: (persist) => {
+        var list = Array.from(Array(100000)).map((x, index) => index)
+      }
+    },
+    {
+      description: 'Maneira 4',
+      amount: 1000,
+      testFunc: (persist) => {
+        var list = Array.apply(null, Array(100000)).map((x, index) => index)
+      }
+    }
   ]
 }
 const results = runBenchmark(aBenchmark)
+
+var fs = require('fs');
+var outputFilename = './results/' + results.description + '.json';
+
+fs.writeFile(outputFilename, JSON.stringify(results, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+});
